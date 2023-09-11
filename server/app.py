08c7671bf7b23bb2e657390ab5631084bfafe4a2
= <=>
2023-09-10 22:42:25 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 CORS(app)
@@ -14,7 +14,8 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-app.route("/messages", methods=["GET", "POST"])
+
+@app.route("/messages", methods=["GET", "POST"])
 def messages():
     if request.method == "GET":
         msgs = []
